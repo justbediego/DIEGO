7d77443f4f25a46a0dd51e5f38b044f96222cf22
justbediego <diegocompleted@gmail.com>
2022-04-28 17:51:02 +0200

    I seem to have a clue on what I'm doing

--- brain.py
+++ brain.py
@@ -12,13 +12,38 @@ class Neuron:
     def __init__(self, nid, is_real=False):
         self.nid = nid
         self.is_real = is_real
-        self.state = 0
         self.dendrites = []
+        self.forward = 0
+        self.backward = 0
 
     def mutate(self, neurons):
         population = [i for i in neurons.keys()]
         random.shuffle(population)
         self.dendrites = [Dendrite(population[i]) for i in range(random.randint(2, 3))]
+
+    def doForward(self, neurons):
+        forward = 0
+        for d in self.dendrites:
+            other = neurons[d.from_nid]
+            forward = forward + (other.forward * d.weight)
+        # activation (relu)
+        forward = forward if forward > 0 else 0
+        if self.is_real:
+            self.backward = self.forward - forward
+        else:
+            self.forward = forward
+
+    def doBackward(self, neurons):
+        if self.backward == 0:
+            return
+        learning_rate = .1
+        for d in self.dendrites:
+            other = neurons[d.from_nid]
+            if other.forward > 0:
+                charge = self.backward * d.weight
+                d.weight = d.weight + (charge * learning_rate)
+                if not other.is_real:
+                    other.backward = charge
 
 
 class Brain:
@@ -29,11 +54,19 @@ class Brain:
             self.neurons[i] = Neuron(i)
         for nid in self.neurons:
             self.neurons[nid].mutate(self.neurons)
-        a = 12
 
     def applyState(self, state):
         for i in range(self.world_size):
-            self.neurons[i].state = state[i]
+            self.neurons[i].forward = state[i]
 
     def thinkOnce(self):
-        print(self.world_size)
+        population = [i for i in self.neurons.keys()]
+        # forward
+        random.shuffle(population)
+        for p in population:
+            self.neurons[p].doForward(self.neurons)
+
+        # backward
+        random.shuffle(population)
+        for p in population:
+            self.neurons[p].doBackward(self.neurons)
